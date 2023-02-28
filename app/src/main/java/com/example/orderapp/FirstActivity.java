2c4e1050ee41d6f0b58fbd0c05b6d6057030db56
viennguyen <viennguyen.280202@gmail.com>
2023-03-01 00:02:10 +0700

    Implement Ui page First, Home and Serve

---
+++ app/src/main/java/com/example/orderapp/FirstActivity.java
@@ -0,0 +1,27 @@
+package com.example.orderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Button button = findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirstActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
