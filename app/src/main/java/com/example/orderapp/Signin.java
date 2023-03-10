4ba97759c527b29bf6c7e692510c13ea39d7170d
SelinaNguyen <SelinaNguyen@SelenaNguyens-MacBook-Air.local>
2023-03-10 15:13:51 +0700

    3:13pm 10/3/2023

--- app/src/main/java/com/example/orderapp/Signin.java
+++ app/src/main/java/com/example/orderapp/Signin.java
@@ -2,7 +2,12 @@ package com.example.orderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.orderapp.view.FirstActivity;
 
 public class Signin extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class Signin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        Button button = findViewById(R.id.btn_snin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signin.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
