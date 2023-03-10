cee71e74a13fbb0605a7e006dae7ab3d9062d69d
SelinaNguyen <SelinaNguyen@SelenaNguyens-MacBook-Air.local>
2023-03-10 07:45:56 +0700

    7:45am 10/3/2023

--- app/src/main/java/com/example/orderapp/view/account.java
+++ app/src/main/java/com/example/orderapp/view/account.java
@@ -1,4 +1,4 @@
-package com.example.orderapp;
+package com.example.orderapp.view;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.orderapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
