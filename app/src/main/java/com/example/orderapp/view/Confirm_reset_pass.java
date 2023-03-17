34289eb80c5a30de957d61966bc0c77afd7e9e2a
SelinaNguyen <SelinaNguyen@SelenaNuyensAir.lan>
2023-03-17 20:21:27 +0700

    done basic ui

--- app/src/main/java/com/example/orderapp/view/Confirm_reset_pass.java
+++ app/src/main/java/com/example/orderapp/view/Confirm_reset_pass.java
@@ -10,26 +10,26 @@ import android.widget.TextView;
 
 import com.example.orderapp.R;
 
-public class Reset_pass extends AppCompatActivity {
+public class Confirm_reset_pass extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset_pass);
-        Button button = findViewById(R.id.btn_send);
+        setContentView(R.layout.activity_confirm_reset_pass);
+        Button button = findViewById(R.id.btn_Confirm);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(Reset_pass.this, Confirm_reset_pass.class);
+                Intent intent1 = new Intent(Confirm_reset_pass.this, Signin.class);
                 startActivity(intent1);
             }
         });
 
-        TextView button_txt_snin = findViewById(R.id.textView_SignIn);
+        TextView button_txt_snin = findViewById(R.id.textView_CFSignIn);
         button_txt_snin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(Reset_pass.this, Signin.class);
+                Intent intent2 = new Intent(Confirm_reset_pass.this, Signin.class);
                 startActivity(intent2);
             }
         });
